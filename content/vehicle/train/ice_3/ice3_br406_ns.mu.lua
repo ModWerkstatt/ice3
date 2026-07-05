c92987801f2436d860db8f691acb5f19b9fd65f5
Dylan7654 <dylan.kreuzinger@gmail.com>
2026-07-05 16:44:35 +0200

    MU ICE 3 NS korrigiert

--- content/vehicle/train/ice_3/ice3_br406_ns.mu.lua
+++ content/vehicle/train/ice_3/ice3_br406_ns.mu.lua
@@ -1,17 +1,17 @@
 ﻿function data()
 return {
 	vehicles = {
-		{ name = "apmzf406_0_NS.mdl", forward = true },
-        { name = "avmz406_1_NS.mdl", forward = true },
-        { name = "bvmz406_2_NS.mdl", forward = false },
-        { name = "brmz406_3_NS.mdl", forward = false },
-        { name = "bpmbz406_8_NS.mdl", forward = true },
-        { name = "bpmz406_7_NS.mdl", forward = true },
-        { name = "bpmz406_6_NS.mdl", forward = false },
-        { name = "bpmzf406_5_NS.mdl", forward = false },
+		{ name = "apmzf406_0_ns.mdl", forward = true },
+        { name = "avmz406_1_ns.mdl", forward = true },
+        { name = "bvmz406_2_ns.mdl", forward = false },
+        { name = "brmz406_3_ns.mdl", forward = false },
+        { name = "bpmbz406_8_ns.mdl", forward = true },
+        { name = "bpmz406_7_ns.mdl", forward = true },
+        { name = "bpmz406_6_ns.mdl", forward = false },
+        { name = "bpmzf406_5_ns.mdl", forward = false },
 	},
-	name = _("ICE3_BR406_1_NS_name"),
-	desc = _("ICE3_BR406_1_NS_desc"),
+	name = _("ICE3_BR406_1_ns_name"),
+	desc = _("ICE3_BR406_1_ns_desc"),
     groupFileName = "menu_ice_3.mdl",
 	filterTags = { "default" },
 }
--- content/vehicle/train/ice_3/ice3_br406_ns_fake.mu.lua
+++ content/vehicle/train/ice_3/ice3_br406_ns_fake.mu.lua
@@ -1,17 +1,17 @@
 ﻿function data()
 return {
 	vehicles = {
-		{ name = "apmzf406_0_NS_fake.mdl", forward = true },
-        { name = "avmz406_1_NS_fake.mdl", forward = true },
-        { name = "bvmz406_2_NS_fake.mdl", forward = false },
-        { name = "brmz406_3_NS_fake.mdl", forward = false },
-        { name = "bpmbz406_8_NS_fake.mdl", forward = true },
-        { name = "bpmz406_7_NS_fake.mdl", forward = true },
-        { name = "bpmz406_6_NS_fake.mdl", forward = false },
-        { name = "bpmzf406_5_NS_fake.mdl", forward = false },
+		{ name = "apmzf406_0_ns_fake.mdl", forward = true },
+        { name = "avmz406_1_ns_fake.mdl", forward = true },
+        { name = "bvmz406_2_ns_fake.mdl", forward = false },
+        { name = "brmz406_3_ns_fake.mdl", forward = false },
+        { name = "bpmbz406_8_ns_fake.mdl", forward = true },
+        { name = "bpmz406_7_ns_fake.mdl", forward = true },
+        { name = "bpmz406_6_ns_fake.mdl", forward = false },
+        { name = "bpmzf406_5_ns_fake.mdl", forward = false },
 	},
-	name = _("ICE3_BR406_1_NS_fake_name"),
-	desc = _("ICE3_BR406_1_NS_desc"),
+	name = _("ICE3_BR406_1_ns_fake_name"),
+	desc = _("ICE3_BR406_1_ns_desc"),
     groupFileName = "menu_ice_3_fake.mdl",
 	filterTags = { "default" },
 }
